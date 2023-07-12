3899f8859b08f7a980ab811cbf14f790d6058fa9
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-07-12 13:59:42 -0500

    Added a test

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftConcurrencyOnLinuxIsBroken",
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,15 +15,25 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", branch: "main"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftConcurrencyOnLinuxIsBroken",
-            dependencies: []),
+            dependencies: [
+            ]
+        ),
         .testTarget(
             name: "SwiftConcurrencyOnLinuxIsBrokenTests",
-            dependencies: ["SwiftConcurrencyOnLinuxIsBroken"]),
-    ]
+            dependencies: [
+                "SwiftConcurrencyOnLinuxIsBroken",
+                "TransmissionAsync",
+                .product(name: "Logging", package: "swift-log"),
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
--- Tests/SwiftConcurrencyOnLinuxIsBrokenTests/SwiftConcurrencyOnLinuxIsBrokenTests.swift
+++ Tests/SwiftConcurrencyOnLinuxIsBrokenTests/SwiftConcurrencyOnLinuxIsBrokenTests.swift
@@ -1,11 +1,34 @@
 import XCTest
 @testable import SwiftConcurrencyOnLinuxIsBroken
+import TransmissionAsync
+
+#if os(macOS)
+import os.log
+#else
+import Logging
+#endif
 
 final class SwiftConcurrencyOnLinuxIsBrokenTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftConcurrencyOnLinuxIsBroken().text, "Hello, World!")
+    func testAcceptTask() throws
+    {
+        let asyncWorks = XCTestExpectation(description: "async task called")
+
+        Task
+        {
+            #if os(macOS)
+            let logger = Logger(subsystem: "testing", category: "testAcceptTask")
+            #else
+            let logger = Logger(label: "testing")
+            #endif
+            let listener = try AsyncTcpSocketListener(port: 1234, logger)
+            let _ = try await listener.accept()
+        }
+
+        Task
+        {
+            asyncWorks.fulfill()
+        }
+
+        wait(for: [asyncWorks])
     }
 }
